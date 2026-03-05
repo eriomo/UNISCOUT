952ccc9bb41aa80c805ec1056e16cf1b5ba953c5
eriomo <trigxyfn@gmail.com>
2026-03-05 12:01:18 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+app.use(express.static(join(__dirname, 'dist')));
+app.get('*', (req, res) => res.sendFile(join(__dirname, 'dist', 'index.html')));
+app.listen(process.env.PORT || 10000, () => console.log('UniScout running'));
